4f425bd3f113e30946b49e270a0a767dad908e5e
Kaso <kaso@pop-os.localdomain>
2023-03-11 13:34:08 -0300

    initial commit

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,50 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Chromium from 'chrome-aws-lambda'
 
-type Data = {
-  name: string
+export type Data = {
+  title: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  let browser = null
+  let result = null
+
+  const url = req.query['url'] || 'https://example.com'
+
+  try {
+    browser = await Chromium.puppeteer.launch({
+      args: Chromium.args,
+      defaultViewport: Chromium.defaultViewport,
+      executablePath: await Chromium.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    })
+
+    const page = await browser.newPage()
+    if (Array.isArray(url)) {
+      await page.goto(url[0])
+    } else {
+      await page.goto(url)
+    }
+
+    result = await page.title()
+
+  } catch (error) {
+    console.error(error)
+
+  } finally {
+    if (browser !== null) {
+      browser.close()
+    }
+  }
+
+  if (result !== null) {
+    res.status(200).json({ title: result })
+  } else {
+    res.status(500).json({ title: '' })
+  }
 }
